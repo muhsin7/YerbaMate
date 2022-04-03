f256917ebda03f7be3aa7b4065876f0595bde261
muhsin <muhsin.ma.mohamed@gmail.com>
2022-04-03 03:10:07 +0100

    node app initialized

--- yerba_mate/lib/main.dart
+++ yerba_mate/lib/main.dart
@@ -7,12 +7,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {    
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Yerba Mate',
@@ -65,7 +64,6 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -81,39 +79,38 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          children: [
-            UserCard(user: "Test"),
-          ],
-        )
-        
-        ),
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        // child: Column(
-        //   // Column is also a layout widget. It takes a list of children and
-        //   // arranges them vertically. By default, it sizes itself to fit its
-        //   // children horizontally, and tries to be as tall as its parent.
-        //   //
-        //   // Invoke "debug painting" (press "p" in the console, choose the
-        //   // "Toggle Debug Paint" action from the Flutter Inspector in Android
-        //   // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-        //   // to see the wireframe for each widget.
-        //   //
-        //   // Column has various properties to control how it sizes itself and
-        //   // how it positions its children. Here we use mainAxisAlignment to
-        //   // center the children vertically; the main axis here is the vertical
-        //   // axis because Columns are vertical (the cross axis would be
-        //   // horizontal).
-        //   // mainAxisAlignment: MainAxisAlignment.center,
-        //   // children: UserCard(name : "Test"),
-        // ),
+          child: Column(
+        children: [
+          UserCard(user: "Linus"),
+        ],
+      )),
+      // Center is a layout widget. It takes a single child and positions it
+      // in the middle of the parent.
+      // child: Column(
+      //   // Column is also a layout widget. It takes a list of children and
+      //   // arranges them vertically. By default, it sizes itself to fit its
+      //   // children horizontally, and tries to be as tall as its parent.
+      //   //
+      //   // Invoke "debug painting" (press "p" in the console, choose the
+      //   // "Toggle Debug Paint" action from the Flutter Inspector in Android
+      //   // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+      //   // to see the wireframe for each widget.
+      //   //
+      //   // Column has various properties to control how it sizes itself and
+      //   // how it positions its children. Here we use mainAxisAlignment to
+      //   // center the children vertically; the main axis here is the vertical
+      //   // axis because Columns are vertical (the cross axis would be
+      //   // horizontal).
+      //   // mainAxisAlignment: MainAxisAlignment.center,
+      //   // children: UserCard(name : "Test"),
       // ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // ),
+    //   floatingActionButton: FloatingActionButton(
+    //     onPressed: _incrementCounter,
+    //     tooltip: 'Increment',
+    //     child: const Icon(Icons.add),
+    //   ), // This trailing comma makes auto-formatting nicer for build methods.
+    // );
     );
   }
 }
